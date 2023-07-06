ac0cc6fbeb33b98f654dfaf4d3110c0652a3664d
otega99 <83589929+otega99@users.noreply.github.com>
2023-07-06 14:16:37 +0000

    Finished Basic React!!!

---
+++ tutorial/src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+  {
+    author: "Taylor Jenkins Reid",
+    title: "The Seven Husbands of Evelyn Hugo",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Rebecca Yarros",
+    title: "Fourth Wing (The Empyrean, 1)",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Johnny Joey Jones",
+    title: "Unbroken Bonds of Battle",
+    img: img3,
+    id: 3,
+  },
+];
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,29 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-const author = "Taylor Jenkins Reid";
-const title = "The Seven Husbands of Evelyn Hugo: A Novel";
-const img = "./images/book-1.jpg";
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  );
-};
-
-const Book = () => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>Amazon Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book key={book.id} {...book} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
